781f56c76175c6e7c64ed8381b7b7d67e8042e6f
Flávio Juvenal <flaviojuvenal@gmail.com>
2014-01-24 16:47:43 -0300

    Interactive tests

--- module/tests/interactive.js
+++ module/tests/interactive.js
@@ -1,19 +1,48 @@
 module("tcp");
 
-asyncTest("Attempt to show an alert with no text", 1, function() {
-	forge.tcp.showAlert("Hello, testing world!", function () {
-		askQuestion("Did you see an alert with the message 'Hello, testing world!'?", {
-			Yes: function () {
-				ok(true, "User claims success");
-				start();
-			},
-			No: function () {
-				ok(false, "User claims failure");
-				start();
-			}
-		});
-	}, function () {
-		ok(false, "API method returned failure");
-		start();
-	});
+asyncTest("Send alice.txt to an echo server and check echo data", 1, function() {
+  var ip = prompt("Please insert the echo server IP " +
+    "(if this is an android emulator, 10.0.2.2 is the host localhost IP)", "10.0.2.2");
+  var port = parseInt(prompt("Please insert the echo server port", "7"), 10);
+  if (ip == null || isNaN(port)) {
+    ok(false, "Please insert a valid ip and port");
+    start();
+    return;
+  }
+  var file = forge.inspector.getFixture("tcp", "alice.txt");
+
+  forge.tcp.test.base64(file, function (base64String) {
+    var echoedData = [];
+    var echoedDataTotal = 0;
+
+    var socket = new forge.tcp.Socket(ip, port, {
+      onError: function (error) {
+        ok(false, "unexpected error: " + JSON.stringify(error));
+        start();
+      }
+    });
+    socket.connect();
+    socket.send(base64String);
+    socket.flush();
+
+    var processData = function (data) {
+      echoedData.push(data);
+      echoedDataTotal += data.length;
+      
+      if (echoedDataTotal < base64String.length) {
+        socket.read(processData);
+      } else if (echoedDataTotal > base64String.length) {
+        socket.close();
+        ok(false, "received more data than was sent");
+        start();
+      } else {
+        socket.close();
+        var echo = echoedData.join('');
+        strictEqual(echo, base64String, "received string was equal to sent string");
+        start();
+      }
+    };
+
+    socket.read(processData);
+  });
 });
